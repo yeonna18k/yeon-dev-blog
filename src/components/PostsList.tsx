025cffec921f969d81c28bfcb5162f2488c3ed30
yeonna18k <yeonna18k@gmail.com>
2024-12-18 10:26:47 +0900

    feat: 썸네일 이미지 로드 로직 수정

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -14,16 +14,16 @@ export default async function PostsList() {
             className="block p-4 border rounded-lg shadow-md hover:shadow-lg transition-shadow"
           >
             <div className="flex">
-              <Image
-                src={
-                  post?.slug
-                    ? `/images/posts/${post.slug}/thumbnail.png`
-                    : `/images/base-img.png`
-                }
-                alt={post.slug || `base-img`}
-                width={300}
-                height={160}
-              />
+              {post.hasThumbnail ? (
+                <Image
+                  src={`/images/posts/${post.slug}/thumbnail.png`}
+                  alt={post.slug || `base-img`}
+                  width={200}
+                  height={150}
+                />
+              ) : (
+                <></>
+              )}
               <div>
                 <div>{post.metadata.title}</div>
                 <div>{post.metadata.date}</div>
